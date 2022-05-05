50041edb72cb232e42d56aa3323b4cdbb42cc411
snomiao <snomiao@gmail.com>
2022-05-05 11:18:00 +0800

    push twice

--- deploy-push.bat
+++ deploy-push.bat
@@ -2,4 +2,5 @@
 git branch --show-current | grep main ^
     || (echo Run this only in main branch && exit 1)
 
+robocopy .\deploy \\sits\SIT-VPN-SERVICE /MIR || ^
 robocopy .\deploy \\sits\SIT-VPN-SERVICE /MIR
